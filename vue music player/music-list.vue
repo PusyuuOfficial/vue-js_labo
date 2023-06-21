6d865630e3adbe4dceba875954c44e8ad162e982
Pusyuu <121094209+PusyuuOfficial@users.noreply.github.com>
2023-06-21 18:12:57 +0900

    Add files via upload

--- vue music player/music-list.vue
+++ vue music player/music-list.vue
@@ -5,23 +5,37 @@ new Vue({
   el: '#app',
   data: {
     music: {
-      title: 'Wave Anime Radioラジオ',
+      title: 'ON Popラジオ',
       artist: '不明',
-      src: 'https://otsu.s04.radio-tochka.com:5475/mount',
+      src: 'https://0n-pop.radionetz.de/0n-pop.mp3',
       pic: ''
     },
     list: [
       {
-        title: "Wave Anime Radioラジオ",
+        title: "ON Popラジオ",
         artist: "不明",
-        src: "https://otsu.s04.radio-tochka.com:5475/mount",
+        src: "https://0n-pop.radionetz.de/0n-pop.mp3",
+        pic: "",
+        lrc: ""
+      },
+      {
+        title: "The Metalラジオ",
+        artist: "不明",
+        src: "https://ais-sa2.cdnstream1.com/1987_128.mp3",
         pic: "",
         lrc: ""
       },
       {
         title: "90s90s BEATラジオ",
         artist: "不明",
-        src: "https://ais-sa2.cdnstream1.com/1987_128.mp3",
+        src: "https://regiocast.streamabc.net/regc-90s90smain4920117-mp3-192-6232809",
+        pic: "",
+        lrc: ""
+      },
+      {
+        title: "Wave Anime Radioラジオ",
+        artist: "不明",
+        src: "https://otsu.s04.radio-tochka.com:5475/mount",
         pic: "",
         lrc: ""
       },
