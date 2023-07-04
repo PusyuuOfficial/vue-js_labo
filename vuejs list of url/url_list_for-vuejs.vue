d869cb68f39680b1bf6bf82aa1e4b9308af821bf
Pusyuu <121094209+PusyuuOfficial@users.noreply.github.com>
2023-07-04 11:34:14 +0900

    プレイリストVideoの改造したやつ

---
+++ vuejs list of url/url_list_for-vuejs.vue
@@ -0,0 +1,28 @@
+    new Vue({
+      el: '#app',
+      data() {
+        return {
+          videos: [
+            {
+              title: "Video 1",
+              src: "https://21emon.wjg.jp/AndroidApp/index.html"
+            },
+            {
+              title: "Video 2",
+              src: "https://21emon.wjg.jp/ClockWithTimeSignal/index.html"
+            },
+            {
+              title: "Video 3",
+              src: "https://chat.openai.com"
+            }
+          ]
+        };
+      },
+      methods: {
+        changeVideo(index) {
+          // ビデオのURLを切り替える処理
+          // 例えば、別のビデオを再生する場合は以下のようにします
+          this.videos[index].src = "http://21emon.wjg.jp";
+        }
+      }
+    });
